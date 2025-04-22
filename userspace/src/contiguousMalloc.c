b09ba15ed86860697bb4c7b2f6f5c88f0547d49f
Jonas Kaufmann <jokau03@gmail.com>
2025-04-22 15:47:43 +0200

    contiguousMalloc.c: optimize opening cma malloc device every time

--- userspace/src/contiguousMalloc.c
+++ userspace/src/contiguousMalloc.c
@@ -21,16 +21,20 @@
 
 #define maybe_printf(fmt, ...) do { if (DEBUGPRINT) fprintf(stderr, fmt, __VA_ARGS__); } while (0)
 
+static int cma_dev_fd = -1;
+
 void* mallocContiguous(const size_t size, uintptr_t* const phys_addr){
     struct cma_space_request_struct req = {
         .size = size
     };
-    int fd = open(CMA_MALLOC_DEVICE_COMPLETE_FILENAME, O_RDWR);
-    if (fd < 0){
+    if (cma_dev_fd == -1) {
+        cma_dev_fd = open(CMA_MALLOC_DEVICE_COMPLETE_FILENAME, O_RDWR);
+    }
+    if (cma_dev_fd < 0){
         maybe_printf("Open failed! Error: %d (%s)\n", errno, strerror(errno));
         return NULL;
     }
-    if (ioctl(fd, CMA_MALLOC_ALLOC, &req) != 0){
+    if (ioctl(cma_dev_fd, CMA_MALLOC_ALLOC, &req) != 0){
         maybe_printf("ioctl CMA_MALLOC_ALLOC failed: %d (%s)\n", errno, strerror(errno));
         return NULL;
     }
@@ -41,15 +45,12 @@ void* mallocContiguous(const size_t size, uintptr_t* const phys_addr){
             size,
             PROT_READ|PROT_WRITE,
             MAP_SHARED,
-            fd,
+            cma_dev_fd,
             req.real_addr
             );
     if (map_addr == MAP_FAILED){
         maybe_printf("mmap failed: %d (%s)\n", errno, strerror(errno));
         return NULL;
-    }
-    if (close(fd)){
-        maybe_printf("Closing the file descriptor failed: %d (%s)", errno, strerror(errno));
     }
     return map_addr;
 }
@@ -63,15 +64,16 @@ int freeContiguous(const uintptr_t phys_addr, void* const ptr, const size_t leng
         maybe_printf("Munmap failed: %d (%s)\n", errno, strerror(errno));
         return -1;
     }
-    int fd = open(CMA_MALLOC_DEVICE_COMPLETE_FILENAME, O_RDWR);
-    if (fd < 0){
+    if (cma_dev_fd == -1) {
+        cma_dev_fd = open(CMA_MALLOC_DEVICE_COMPLETE_FILENAME, O_RDWR);
+    }
+    if (cma_dev_fd < 0){
         maybe_printf("Open failed! Error: %d (%s)\n", errno, strerror(errno));
         return -1;
     }
-    if (ioctl(fd, CMA_MALLOC_FREE, &req) != 0){
+    if (ioctl(cma_dev_fd, CMA_MALLOC_FREE, &req) != 0){
         maybe_printf("ioctl CMA_MALLOC_FREE failed: %d(%s)\n", errno, strerror(errno));
         return -1;
     }
-    close(fd);
     return 0;
 }
